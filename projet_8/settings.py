e154594efde5a4bee81e77cc4f8ab36d721b7157
Axel-V <axel.vallee@icloud.com>
2019-03-20 19:03:40 +0100

    added templates for login view and account view

--- projet_8/settings.py
+++ projet_8/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -104,3 +104,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
--- user/forms.py
+++ user/forms.py
@@ -1,13 +1,20 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, \
+    AuthenticationForm, \
+    UsernameField
+from django.utils.translation import gettext, gettext_lazy as _
 from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False,
-                                 help_text='Optionnel.', label='Prénom')
-    last_name = forms.CharField(max_length=30, required=False,
-                                help_text='Optionnel.', label='Nom')
+    first_name = forms.CharField(max_length=30,
+                                 required=False,
+                                 help_text='Optionnel.',
+                                 label='Prénom')
+    last_name = forms.CharField(max_length=30,
+                                required=False,
+                                help_text='Optionnel.',
+                                label='Nom')
     email = forms.EmailField(max_length=254, )
 
     class Meta():
@@ -16,7 +23,16 @@ class SignUpForm(UserCreationForm):
             'username', 'last_name', 'first_name', 'email', 'password1',
             'password2',)
 
+
 class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs=
+                                                    {'autofocus': True,
+                                                     'class': 'form-control'}))
+    password = forms.CharField(
+            label=_("Password"),
+            strip=False,
+            widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    )
 
     class Meta():
         model = User
